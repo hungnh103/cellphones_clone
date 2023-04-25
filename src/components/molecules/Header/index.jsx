dabf40503b5c6324584bed92ac55058bedfa84c6
Nguyễn Huy Hùng <nguyen.huy.hung-c@sun-asterisk.com>
2023-04-25 17:54:27 +0700

    feat: Add some basic element into header

--- src/components/molecules/Header/index.jsx
+++ src/components/molecules/Header/index.jsx
@@ -1,9 +1,30 @@
 import 'styles/components/header.scss'
 
+import { useTranslation } from 'react-i18next'
+
 const Header = () => {
+  const { t, i18n } = useTranslation()
+
   return (
     <header>
-      Header component
+      <div className='header-wrapper'>
+        <div className='header-wrapper__logo'>
+          <img src="images/common/logo.png" alt="logo" />
+        </div>
+
+        <form>
+          <input type="text" placeholder={t('components.header.form.input_search.placeholder')} />
+        </form>
+
+        <select onChange={e => { i18n.changeLanguage(e.target.value) }}>
+          <option value="vi">Tiếng Việt</option>
+          <option value="en">English</option>
+        </select>
+
+        <button>
+          {t('components.header.login')}
+        </button>
+      </div>
     </header>
   )
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
 
+import './i18n'
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Suspense fallback='loading'>
+      <App />
+    </Suspense>
   </React.StrictMode>
 );
