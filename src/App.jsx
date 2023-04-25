195d8b27685835bab49fc8a3eb4d1058ca14f27e
Nguyễn Huy Hùng <49478611+hungnh-0828@users.noreply.github.com>
2023-04-25 15:50:26 +0700

    feat: Add skeleton for some pages (#4)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
+import Home from "pages/Home";
+import Product from "pages/Product";
+import Cart from "pages/Cart";
+import Category from "pages/Category";
+import NotFound from "pages/NotFound";
+import Header from "components/molecules/Header";
+
+import 'styles/app.scss'
+
 function App() {
   return (
     <div className="App">
-      App component
+      <Header />
+
+      <main>
+        <div className="body-wrapper">
+          <Router>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/categories/:id" element={<Category />} />
+              <Route path="/products/:id" element={<Product />} />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </Router>
+        </div>
+      </main>
     </div>
   );
 }
