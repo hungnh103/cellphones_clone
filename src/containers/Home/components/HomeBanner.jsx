fb6c6df871105ea18b0ef7993bfb94b8a1efee41
Nguyễn Huy Hùng <nguyen.huy.hung-c@sun-asterisk.com>
2023-04-29 12:23:59 +0700

    feat: Add banner; Remove some unused borders

--- src/containers/Home/components/HomeBanner.jsx
+++ src/containers/Home/components/HomeBanner.jsx
@@ -1,7 +1,9 @@
+import 'styles/containers/home/home_banner.scss'
+
 const HomeBanner = () => {
   return (
-    <div>
-      HomeBanner component
+    <div className="home-banner-wrapper">
+      <img src="https://cdn2.cellphones.com.vn/1200x75,webp,q100/https://dashboard.cellphones.com.vn/storage/banner-special-desktop-pkchaohe.png" alt="banner" />
     </div>
   )
 }
